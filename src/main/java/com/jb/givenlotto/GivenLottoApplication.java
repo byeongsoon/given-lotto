7c59718747e7a3155693dfcc2fc181576c558145
byeongsoon <byeongsoon94@gmail.com>
2023-05-25 21:51:21 +0900

    setting : 프로젝트 생성 및 연결

---
+++ src/main/java/com/jb/givenlotto/GivenLottoApplication.java
@@ -0,0 +1,13 @@
+package com.jb.givenlotto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GivenLottoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GivenLottoApplication.class, args);
+    }
+
+}
